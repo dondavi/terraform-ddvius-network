35c95ed6886bf2cdf8beecb3d0a8655d00887ba5
David Sanchez <david@dondavi.com>
2020-02-20 10:09:44 -0600

    Update providers.tf

--- network_tmp/providers.tf
+++ network_tmp/providers.tf
@@ -6,14 +6,3 @@ provider "aws" {
   profile  =  "${var.profile}"
 }
 */
-
-// Import Issue
-
-provider "aws" {
-  region     = "us-east-2"
-  access_key = "AKIAJ6WAOG6XE76HBVCA"
-  secret_key = "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"
-
-}
-
-
